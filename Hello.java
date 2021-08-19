90f5bdbc0671783c1190552e6e6612f7d5858d18
Enda Crossan <48137079+endacrossan@users.noreply.github.com>
2021-08-19 08:32:57 +0100

    removed capital

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-Public class Hello {
+public class Hello {
    public static void main(String[] args) {
       // Hello world in Java used to test Jenkins git integration
       System.out.println("Hello World!");
