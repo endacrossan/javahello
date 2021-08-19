c93f2c5f7c287d1c9339010b52e06c0af3355228
Enda Crossan <48137079+endacrossan@users.noreply.github.com>
2021-08-19 08:15:47 +0100

    Hello World to test Jenkins/Git integration

---
+++ Hello.java
@@ -0,0 +1,6 @@
+ublic class Hello {
+   public static void main(String[] args) {
+      // Hello world in Java used to test Jenkins git integration
+      System.out.println("Hello World!");
+   }
+}
