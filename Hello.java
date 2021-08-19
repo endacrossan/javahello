b2ff386f64832eb2ead73cf8a41b843e81fd5609
Enda Crossan <48137079+endacrossan@users.noreply.github.com>
2021-08-19 08:31:44 +0100

    Fixed typo on Public class

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-ublic class Hello {
+Public class Hello {
    public static void main(String[] args) {
       // Hello world in Java used to test Jenkins git integration
       System.out.println("Hello World!");
